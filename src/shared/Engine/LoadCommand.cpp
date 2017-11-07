9f5708fb18dfd0d896bb8409eb5d3b8837207c7e
Sebachar <sebastien.charrier@ensea.fr>
2017-11-07 12:01:43 +0100

    LoadCommand.cpp

--- src/shared/Engine/LoadCommand.cpp
+++ src/shared/Engine/LoadCommand.cpp
@@ -25,7 +25,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
 #include "LoadCommand.h"
+
 
 
 namespace Engine
@@ -38,20 +40,84 @@ namespace Engine
 	
 	void LoadCommand::Execute(Etat::State& state)
 	{
-		std::ifstream FichierJ1(deck1 + ".deck");
-		std::string NomCarte;
+		std::string Type;
+		std::string Nom;
+		std::string Cout;
+		std::string Force;
+		std::string Endurance;
+		std::string NbCapa;
+		std::string Ability;
+		std::string Texte;
+		std::string Ligne;
+		std::vector<Etat::Capacite> Capacites;
+		int id=-1;
+		int Joueur;
+		int TailleDeck=0;
 		
 		
-		
-		
-		
-		
-		
+		for(Joueur=0;Joueur<state.GetJoueurs().size();Joueur++)
+		{
+			std::ifstream FichierDeck;
+			if(Joueur==0)
+			{
+				FichierDeck.open("/res/decks/" + deck1 + ".deck");
+			}
+			else
+			{
+				if(Joueur==1)
+				{
+					FichierDeck.open("/res/decks/" + deck2 + ".deck");
+				}
+				else
+				{
+					std::cout << "Beaucoup de joueurs ici" << std::endl;
+					return;
+				}
+			}
+			
+			
+			while(FichierDeck.peek()!=EOF)
+			{
+				TailleDeck++;
+				std::getline(FichierDeck, Nom);
+	
+				std::ifstream Card("/res/cartes" + Nom + ".Carte");
+				
+				std::getline(Card, Type);
+				std::getline(Card, Nom);
+				std::getline(Card, Cout);
+				std::getline(Card, Force);
+				std::getline(Card, Endurance);
+				std::getline(Card, NbCapa);
+				for(int i=0;i<std::stoi(NbCapa);i++)
+				{
+					std::getline(Card, Ability);
+					auto CostAbility = new Etat::Cout();
+					CostAbility->SetCost(Ability[0],Ability[4],Ability[2],Ability[6],Ability[8],Ability[10],Ability[12],Ability[14],Ability[16]);
+					Capacites.push_back(new Etat::Active(*CostAbility,Ability.substr(18,Ability.size()-18),id++,Joueur,Ability.substr(18,Ability.size()-18)));
+				}
+				
+				auto Cost = new Etat::Cout();
+				Cost->SetCost(Cout[0],Cout[4],Cout[2],Cout[6],Cout[8],Cout[10],Cout[12],Cout[14],Cout[16]);
+				if (std::strcmp(Type.data(), "creature")==0)
+				{
+					state.GetJoueurs()[Joueur]->AddCardLibrary(new Etat::Creature(std::stoi(Force),std::stoi(Endurance),false,Nom,Cost,Capacites,id++,Joueur);
+				}
+				else
+				{
+					state.GetJoueurs()[Joueur]->AddCardLibrary(new Etat::Carte(std::strcmp(Type.data(),"sort")!=0,std::strcmp(Type.data()	,"terrain")==0,std::strcmp(Type.data(),"creature")==0,0,Nom,Cost,Capacites,id++,Joueur);
+				}
+				
+				Texte="";
+				while(Card.peek()!=EOF)
+				{
+					getline(Card, Ligne);
+					Texte=Texte + Ligne + "\n";
+				}
+				state.GetJoueurs()[Joueur]->GetLibrary()[TailleDeck]->SetOracle(Texte);			
+				Card.close();
+			}
+			FichierDeck.close();
+		}	
 	}
-		
-	
-	
-	
-	
-	
 }
