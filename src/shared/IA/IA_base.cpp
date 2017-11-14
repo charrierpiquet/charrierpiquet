b31ebdeebfabdc6ee0235cea08e3fbedb60a4b87
Sebachar <sebastien.charrier@ensea.fr>
2017-11-14 09:00:47 +0100

    Corrections IA

--- src/shared/IA/IA_base.cpp
+++ src/shared/IA/IA_base.cpp
@@ -1,6 +1,14 @@
 #include "IA_base.h"
 #include <iostream>
 #include <ctime>
+#include <Engine/CastCommand.h>
+#include <Engine/LetPriorityCommand.h>
+#include <Engine/AttackCommand.h>
+#include <cstdlib>
+#include <ctime>
+
+
+
 
 namespace IA
 {
@@ -19,27 +27,28 @@ namespace IA
 		std::vector<std::shared_ptr<Etat::Carte> > NonLandMainJoueur;
 		std::vector<std::shared_ptr<Etat::Carte> > Board;
 		std::vector<std::shared_ptr<Etat::Carte> > TerrainsJoueur;
-		std::vector<std::shared_ptr<Etat::Carte> > CreaturesJoueur;
+		std::vector<std::shared_ptr<Etat::Creature> > CreaturesJoueur;
 		
 		MainJoueur = currentState->GetJoueurs()[currentState->GetJoueurTour()]->GetHand();
 		Board = currentState->GetBattlefield();
 		int c=0;
-		for( unsigned int i=0;i<Board->size();i++) //Récupération des listes de créatures et terrians controlés par le joueur
+		unsigned int i=0;
+		for(i=0;i<Board.size();i++) //Récupération des listes de créatures et terrians controlés par le joueur
 		{
-			if ((Board[i]->GetIndJoueur() == currentState->GetJoueurTour()) && (Board[i]->isCreature()))
+			if ((Board[i]->GetIndJoueur() == currentState->GetJoueurTour()) && (Board[i]->GetIsCreature()))
 			{
-				CreaturesJoueur[c] = Board[i];
+				CreaturesJoueur[c] = std::static_pointer_cast<Etat::Creature>(Board[i]);
 				c++;
 			}
 			
-			if ((Board[i]->GetIndJoueur() == currentState->GetJoueurTour()) && (Board[i]->isLand()))
+			if ((Board[i]->GetIndJoueur() == currentState->GetJoueurTour()) && (Board[i]->GetIsLand()))
 			{
 				TerrainsJoueur[c] = Board[i];
 				c++;
 			}
 		}
 		c=0;
-		for(i=0;i<MainJoueur->size();i++)
+		for(i=0;i<MainJoueur.size();i++)
 		{
 			if (!MainJoueur[i]->GetIsLand())
 			{
@@ -48,69 +57,71 @@ namespace IA
 			}
 		}
 			
-		
-		switch(currentState->getPhase())
+		std::shared_ptr<Engine::LetPriorityCommand> Past (std::shared_ptr<Engine::LetPriorityCommand>(new Engine::LetPriorityCommand()));
+		switch(currentState->GetPhase())
 		{
 			case 0:
+			{
 			
-				for(i=0;i<MainJoueur->size();i++)
+				for(i=0;i<MainJoueur.size();i++)
 				{
 					if (MainJoueur[i]->GetIsLand())
 					{
 						std::shared_ptr<Engine::CastCommand> Cast (std::shared_ptr<Engine::CastCommand>(new Engine::CastCommand(MainJoueur[i],nullptr,nullptr)));
-                        engine.AddCommand(Cast);
+                        engine->AddCommand(Cast);
                         break;
 					}
 				}
 				
-				std::shared_ptr<Engine::LetPriorityCommand> Past (std::shared_ptr<Engine::LetPriorityCommand>(new Engine::LetPriorityCommand()));
-				while(currentState->getPhase()==0)
+				
+				while(currentState->GetPhase()==0)
 				{
-					engine.AddCommand(Past);
+					engine->AddCommand(Past);
 				}
+				break;
+			}
 			
 			case 1:
-			
+			{
 				std::shared_ptr<Engine::AttackCommand> attaque (std::shared_ptr<Engine::AttackCommand>(new Engine::AttackCommand()));
-				for(i=0;i<CreaturesJoueur->size();i++)
+				for(i=0;i<CreaturesJoueur.size();i++)
 				{
 					attaque->AddAttaquant(CreaturesJoueur[i]);
 				}
-                engine.AddCommand(attaque);
-                while(currentState->getPhase()==1)
+                engine->AddCommand(attaque);
+                while(currentState->GetPhase()==1)
 				{
-					engine.AddCommand(Past);
+					engine->AddCommand(Past);
 				}
 				break;
+			}
 			
 			case 4:
-			
-				for(i=0;i<TerrainsJoueur;i++)
+			{
+				for(i=0;i<TerrainsJoueur.size();i++)
 				{
 					std::shared_ptr<Engine::CastCommand> TapLand (std::shared_ptr<Engine::CastCommand>(new Engine::CastCommand(TerrainsJoueur[i]->GetAbility()[0],TerrainsJoueur[i],nullptr)));
-                    engine.AddCommand(TapLand);
+                    engine->AddCommand(TapLand);
 				}
 				
 				
-				if(NonLandMainJoueur->size()>0)
+				if(NonLandMainJoueur.size()>0)
 				{
-					std::shared_ptr<Engine::CastCommand> Cast (std::shared_ptr<Engine::CastCommand>(new Engine::CastCommand(NonLandMainJoueur[rand_a_b(0,(NonLandMainJoueur->size()-1)],nullptr,nullptr)));
-					engine.AddCommand(Cast);
+					std::shared_ptr<Engine::CastCommand> Cast (std::shared_ptr<Engine::CastCommand>(new Engine::CastCommand(NonLandMainJoueur[rand()%(NonLandMainJoueur.size()-1)],nullptr,nullptr)));
+					engine->AddCommand(Cast);
 				}
 				
-				std::shared_ptr<Engine::LetPriorityCommand> Past (std::shared_ptr<Engine::LetPriorityCommand>(new Engine::LetPriorityCommand()));
-				while(currentState->getPhase()==4)
+				while(currentState->GetPhase()==4)
 				{
-					engine.AddCommand(Past);
+					engine->AddCommand(Past);
 				}
 				break;
-				
-			case default:
-			
-				std::shared_ptr<Engine::LetPriorityCommand> Past (std::shared_ptr<Engine::LetPriorityCommand>(new Engine::LetPriorityCommand()))
-				enigne.AddCommand(Past);
+			}
+			default:
+			{
+				engine->AddCommand(Past);
 				break;
-				
+			}
 		}
 					
        
