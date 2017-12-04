dd8d0119c78bcd97057565f9df828439dcf35af7
piquet <florian.piquet@ensea.fr>
2017-12-04 21:18:42 +0100

    oubli dia

--- src/shared/Etat/State.h
+++ src/shared/Etat/State.h
@@ -59,7 +59,7 @@ namespace Etat {
     std::vector<std::shared_ptr<Creature> > GetListBloque () const;
     int GetInd ();
     void SetTour (int value);
-    void ClearAtt();
+    void ClearAtt ();
     // Setters and Getters
   };
 
